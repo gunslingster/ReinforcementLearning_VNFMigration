d00474aef9d425e9f352dd53f33f183d920cf7da
gunslingster <43232009+gunslingster@users.noreply.github.com>
2021-01-25 12:35:50 -0800

    Update fat_tree.py

--- fat_tree.py
+++ fat_tree.py
@@ -1,24 +1,21 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Jan  4 10:53:40 2021
+Created on Tue Jan 12 10:25:27 2021
 
-@author: Sterling Abrahams
+@author: sabrahams
 """
 
 # Implementation of a fat tree topology 
 
 import math
-from collections import defaultdict
+import random
 
 k = int(input('Enter number of ports for each switch, must be an even number: '))
         
 class fatTree():
     
     CoreSwitchList = []
-    CoreDict = defaultdict(lambda: [])
     AggSwitchList = []
     EdgeSwitchList = []
-    EdgeDict = defaultdict(lambda: [])
     # Each pod will be a list of agg switches and edge switches
     # k/2 agg switchs and k/2 edge switches
     PodList = []
@@ -29,7 +26,8 @@ class fatTree():
     def __init__(self, k):
         self.k = k
         self.podSize = int(k/2)
-        self.iCoreLayerSwitch = int(pow(k/2,2)) 
+        self.iCoreLayerSwitch = int(pow(k/2,2))
+        self.hostsPerPod = self.iCoreLayerSwitch
         self.iAggLayerSwitch = int(k/2) * k
         self.iEdgeLayerSwitch = self.iAggLayerSwitch
         self.iHost = self.iEdgeLayerSwitch * self.podSize
@@ -39,6 +37,7 @@ class fatTree():
         self.createEdgeLayerSwitch(self.iEdgeLayerSwitch)
         self.createHost(self.iHost)
         self.createPod(self.iPods)
+        self.allNodes = self.CoreSwitchList + self.AggSwitchList + self.EdgeSwitchList + self.HostList
         print('Core Switches:')
         print(self.CoreSwitchList)
         print('Aggregation Switches: ')
@@ -111,7 +110,6 @@ class fatTree():
                 aggCount += 1
             for j in range(aggCount, self.iAggLayerSwitch, podSize):
                 self.addLink(self.CoreSwitchList[i], self.AggSwitchList[j])
-                self.CoreDict[self.CoreSwitchList[i]].append(self.AggSwitchList[j])
                 
         # Connect agg switches in each pod to edge switches in the pod
         for pod in self.PodList:
@@ -126,7 +124,6 @@ class fatTree():
         for i in range(self.iEdgeLayerSwitch):
             for j in range(pmcount, pmcount + podSize):
                 self.addLink(self.EdgeSwitchList[i], self.HostList[j])
-                self.EdgeDict[self.EdgeSwitchList[i]].append(self.HostList[j])
             pmcount += podSize
         
             
@@ -135,108 +132,101 @@ class fatTree():
     def calc_dist(self, node1, node2):
         # Can use the IDs and pods to calculate distance on a case by case basis
         
+        # Both nodes are the same
         if node1 == node2:
             return 0
      
-        # First case, both nodes are hosts
+        # Case 1: host-host
         if node1[0] == '4' and node2[0] == '4':
-            for edgeSwitch in self.EdgeSwitchList:
-                if node1 in self.EdgeDict[edgeSwitch] and node2 in self.EdgeDict[edgeSwitch]:
-                    return 2
-            
-            for pod in self.PodList:
-                podCheck = []
-                for switch in pod:
-                    if switch[0] == '3':
-                        for host in self.EdgeDict[switch]:
-                            podCheck.append(host)
-                if node1 in podCheck and node2 in podCheck:
-                    return 4
+            # Both hosts are connected to the same edge switch
+            if math.ceil(int(node1[2:]) / self.podSize) == math.ceil(int(node2[2:]) / self.podSize): 
+                return 2
+            # Both hosts are connected ot the same pod
+            if math.ceil(int(node1[2:]) / self.hostsPerPod) == math.ceil(int(node2[2:]) / self.hostsPerPod): 
+                return 4
+            # Both hosts are in different pods
             return 6
         
-        # Second case, host and edge switch
-        if node1[0] == '4' and node2[0] == '3':
-            check1 = node2
-            check2 = node1
+        # Case 2: host-edge
+        if node1[0] == '4' and node2[0] == '3' or node1[0] == '3' and node2[0] == '4':
+            check1 = node1[2:] if node1[0] == '4' else node2[2:]
+            check2 = node1[2:] if node1[0] == '3' else node2[2:]
             
-            if check1 in self.EdgeDict[check2]:
+            # Host connected to edge
+            if math.ceil(int(check1) / self.podSize) == int(check2):
                 return 1
-        
-            hostsPerPod = self.iCoreLayerSwitch
-            if math.ceil(int(check1[2:])/hostsPerPod) == math.ceil(int(check1[2:])/self.podSize):
+            # Host connected to edge within same pod
+            if math.ceil(int(check1) / self.hostsPerPod) == math.ceil(int(check2) / self.podSize):
                 return 3
             return 5
-        if node1[0] == '3' and node2[0] == '4':
-            check1 = node2
-            check2 = node1
+                                                                          
+        # Case 3: host-agg
+        # Distance is 2 if host connected to that pod, or 4 if not
+        if node1[0] == '4' and node2[0] == '2' or node1[0] == '2' and node2[0] == '4':
+            check1 = node1[2:] if node1[0] == '4' else node2[2:]
+            check2 = node1[2:] if node1[0] == '2' else node2[2:]
             
-            if check1 in self.EdgeDict[check2]:
-                return 1
-        
-            hostsPerPod = self.iCoreLayerSwitch
-            if math.ceil(int(check1[2:])/hostsPerPod) == math.ceil(int(check1[2:])/self.podSize):
-                return 3
-            return 5
-        
-        # Third case, host and agg switch
-        # DIst is 2 if it's connected to that pod, or 4 if not
-        if node1[0] == '4' and node2[0] == '2':
-            check1 = node1
-            check2 = node2
-            hostsPerPod = self.iCoreLayerSwitch
-            if math.ceil(int(check1[2:])/hostsPerPod) == math.ceil(int(check1[2:])/self.podSize):
+            if math.ceil(int(check1) / self.hostsPerPod) == math.ceil(int(check2) / self.podSize):
                 return 2
             return 4
-        if node1[0] == '2' and node2[0] == '4':
-            check1 = node2
-            check2 = node1
-            hostsPerPod = self.iCoreLayerSwitch
-            if math.ceil(int(check1[2:])/hostsPerPod) == math.ceil(int(check1[2:])/self.podSize):
-                return 2
-            return 4
+            
+            
         
-        # Fourth Case, host and core switch, dist is always 3
+        # Case 4: host-core
+        # Distance is always 3
         if node1[0] == '4' and node2[0] == '1' or node1[0] == '1' and node2[0] == '4':
             return 3
             
-        # Fifth case, edge and edge switch
+        # Case 5: edge-edge
+        # Distance is 2 if in same pod, 4 if not
         if node1[0] == '3' and node2[0] == '3':
-            if math.ceil(int(node1[2:])/self.podSize) == math.ceil(int(node2[2:])/self.podSize):
+            if math.ceil(int(node1[2:]) / self.podSize) == math.ceil(int(node2[2:]) / self.podSize):
                 return 2
             else:
                 return 4
         
-        # Sixth case, Edge and Agg switch
+        # Case 6: edge-agg
+        # Distance is 1 if in the same pod, 3 if not
         if node1[0] == '3' and node2[0] == '2' or node1[0] == '2' and node2[0] == '3':
             if math.ceil(int(node1[2:]) / self.podSize) == math.ceil(int(node2[2:]) / self.podSize):
                 return 1
             else:
                 return 3
         
-        # Seventh case, Edge and Core switch, dist is always 2
+        # Case 7: edge-core
+        # Distance always 2
         if node1[0] == '3' and node2[0] == '1' or node1[0] == '1' and node2[0] == '3':
             return 2
         
-        # Case 8, Agg and Agg switch
+        # Case 8: agg-agg
+        # Distance is 2 if they are in the same pod, or connected to the same core switch, otherwise distance is 4
         if node1[0] == '2' and node2[0] == '2':
-            if int(node1[2:]) % self.podSize == int(node2[2:]) % self.podSize or math.ceil(int(node1[2:])/self.podSize) == math.ceil(int(node2[2:])/self.podSize):
+            if int(node1[2:]) % self.podSize == int(node2[2:]) % self.podSize or math.ceil(int(node1[2:]) / self.podSize) == math.ceil(int(node2[2:]) / self.podSize):
                 return 2
             else:
                 return 4
         
-        # Case 9, Agg and Core switch
+        # Case 9: agg-core
+        # Distance is 1 if they are directely connected, 3 if not
         if node1[0] == '2' and node2[0] == '1':
-            if int(node1[2:]) % self.podSize == int(node2[2:]) % self.podSize:
+            if math.ceil(int(node2[2:]) / self.podSize) == self.podSize:
+                if int(node1[2:]) % (self.podSize) == 0:
+                    return 1
+            if int(node1[2:]) % (self.podSize) == math.ceil(int(node2[2:]) / self.podSize):
                 return 1
             else:
                 return 3
         if node1[0] == '1' and node2[0] == '2':
-            if int(node1[2:]) % self.podSize == int(node2[2:]) % self.podSize:
+            if math.ceil(int(node1[2:]) / self.podSize) == self.podSize:
+                if int(node2[2:]) % (self.podSize) == 0:
+                    return 1
+            if int(node2[2:]) % (self.podSize) == math.ceil(int(node1[2:]) / self.podSize):
                 return 1
             else:
                 return 3
         
-        # Case 10, Core and Core switch
+        # Case 10: core-core
+        # Distance is 2 if they are connected to the same agg switches, 4 if not
         if node1[0] == '1'and node2[0] == '1':
             if math.ceil(int(node1[2:]) / self.podSize) == math.ceil(int(node2[2:]) / self.podSize):
                 return 2
@@ -244,11 +234,19 @@ class fatTree():
                 return 4
             
             
+            
 mytree = fatTree(k)
 n1 = input('Please enter first node: ')
 n2 = input('Please enter second node: ')
 print('Distance between nodes is : {}'.format(mytree.calc_dist(n1,n2)))
+
+# Random distance testing
+def distanceCheck(episodes, tree):
+    for i in range(episodes):
+        node1 = random.choice(tree.allNodes)
+        node2 = random.choice(tree.allNodes)
+        distance = mytree.calc_dist(node1, node2)
+        print("Distance between {} and {}: {}".format(node1, node2, distance))
     
-    
-        
-        
+distanceCheck(100, mytree)
+
