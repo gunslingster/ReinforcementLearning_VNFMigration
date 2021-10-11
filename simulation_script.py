dc62c83b03d64aefe5ecacc11a48c51b98f9869a
Sterling <sterlingabrahams@yahoo.com>
2021-10-11 15:45:44 -0700

    Able to compute SFC availability via MCF.

--- simulation_script.py
+++ simulation_script.py
@@ -15,10 +15,28 @@ for num_vnf in range(1, 10, 2):
     for count in range(1, 11):
         flow_network = FlowNetwork(8, num_vnf, 1)
         f = f'{num_vnf}vnf_simulation{count}.txt'
+        output = f'output{num_vnf}{count}.txt'
         path1 = os.path.join(dir1, f)
         path2 = os.path.join(dir2, f)
+        path3 = os.path.join(dir3, output)
         flow_network.gen_output_file(path2)
         flow_network.gen_information_file(path1)
+        os.system(f'./cs2.exe < {path2path} > {path3}')
+        vnf_mapping = extract_mcf(path3)
+        mcf = 1
+        for vnf, backup in vnf_mapping.items():
+            vnf = get_key_from_value(flow_network.node_mapping ,vnf)
+            backup = get_key_from_value(flow_network.node_mapping,backup)
+            vnf_failure = flow_network.failure_probabilities[vnf]
+            backup_failure = flow_network.failure_probabilities[backup]
+            mcf *= 1 - vnf_failure * backup_failure
+        with open(path1, 'a') as f:
+            f.write(f'SFC availability via MCF: {mcf}\n')
+
+
+
+    
+
 
 
     
