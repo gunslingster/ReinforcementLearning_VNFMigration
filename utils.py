64ef214242f135b440ca61ec2c7253a5d724e5e7
Sterling <sterlingabrahams@yahoo.com>
2021-10-11 08:46:49 -0700

    Utility function added to interpret output of mcf algorithm.

---
+++ utils.py
@@ -0,0 +1,16 @@
+def extract_mcf(output_file):
+    mapping = {}
+    with open(output_file) as f:
+        for i in range(15):
+            next(f)
+        for line in f:
+            info = line.split()
+            if info[0] == 'c':
+                continue
+            if info[1] == '0':
+                continue
+            if info[2] == '1':
+                continue
+            if info[3] == '1':
+                mapping[int(info[1])] = int(info[2])
+    return mapping
